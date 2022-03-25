c71f611d4c255fda40bc0446a03625cb5e367427
we-wow <mj09531@163.com>
2022-03-25 20:52:07 +0800

    deleted some old files

--- main.py
+++
@@ -1,9 +0,0 @@
-from utils.graph import Graph
-
-g = Graph(topology_file_path='data/example2.csv')
-# If there isn't a given initial tree, program will find one automatically
-# all_tree_1 = g.find_all_spanning_tree_by_polynomial()
-all_tree_1 = g.find_all_spanning_tree_by_polynomial(initial_tree={1, 2, 5})
-all_tree_2 = g.find_all_spanning_tree_by_iteration()
-print(all_tree_1)
-print(all_tree_2)
