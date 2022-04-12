891d815c610a3234695cc157bcef5eb90ba498ca
we-wow <61772333+we-wow@users.noreply.github.com>
2022-04-12 22:28:40 +0800

    fixed bug of file path

--- FundamentalsToAI/work4/utils/read_pickle_file.py
+++ FundamentalsToAI/work4/utils/read_pickle_file.py
@@ -6,11 +6,12 @@ Read pickle file.
 @Version: 0.1.0
 @Content: First version.
 """
+import os
 import pickle
 from .config import DATA_DIR
 
 
 def read_pickle(file_name: str):
-    with open(f"{DATA_DIR}/pickle/{file_name}", 'rb') as f:
+    with open(os.path.join(DATA_DIR, "pickle", file_name), 'rb') as f:
         variable = pickle.load(f)
     return variable
